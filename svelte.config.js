33e5b79a1e91d7d3d5f288786620f7d83b49cbbb
soren <sorens.77@gmail.com>
2022-05-30 15:40:53 +0300

    notherone

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 const sveltePreprocess = require('svelte-preprocess');
+const adapter = require('@sveltejs/adapter-cloudflare');
+
 
 module.exports = {
+  kit: {
+    adapter: adapter(),
+        // ... truncated ...
+      },
   preprocess: [
     sveltePreprocess({
       postcss: {
